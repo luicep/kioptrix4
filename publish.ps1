e968c433dfd0e1ebfcd911bd84d4e0f3cc51eec7
Luis Cepeda <Luiscepeda44@hotmail.com>
2026-07-23 13:35:13 -0400

    Remove Git initialization commands from publish.ps1
    Removed initial Git commands for portfolio release.

--- publish.ps1
+++ publish.ps1
@@ -1,6 +1,1 @@
-$ErrorActionPreference = "Stop"
-git init -b main
-git add .
-git commit -m "Initial public portfolio release"
-git remote add origin https://github.com/runeforged/kioptrix4.git
-git push -u origin main
+
