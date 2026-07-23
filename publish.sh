83b4f845939e1daf700306b954925f6b5071fb39
Luis Cepeda <Luiscepeda44@hotmail.com>
2026-07-23 13:35:21 -0400

    Remove initial git setup commands from publish.sh

--- publish.sh
+++ publish.sh
@@ -1,8 +1,1 @@
-#!/usr/bin/env bash
-set -euo pipefail
 
-git init -b main
-git add .
-git commit -m "Initial public portfolio release"
-git remote add origin https://github.com/runeforged/kioptrix4.git
-git push -u origin main
